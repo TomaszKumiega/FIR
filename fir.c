f1a28113d8fa78fb25130aa4a1ec6e17a3f2d26d
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-05-25 18:41:29 +0200

    Add C implementation of the filter

--- fir.c
+++ fir.c
@@ -1,7 +1,36 @@
+#include <stdio.h>
 #include "fir.h"
+
+double* fir(double input[],int inputLength);
 
 void main()
 {
-	double input[500];
+	double input[242] = {32767};
+	double *output;
+	output = fir(input,242);
+
+	for(int i=0; i<242; i++)
+	{
+	    printf("Coeffs: %f \n", coeffs[i]*32767);
+		printf("Output: %f \n", *(output+i));
+	}
 	
+}
+
+double* fir(double input[],int inputLength)
+{
+	double outputValue=0;
+	static double output[242];
+
+	for(int k=0;k<inputLength;k++)
+	{
+		for(int i=0;i<no_of_coeffs;i++)
+		{
+			if(inputLength-k-i>-1) outputValue=outputValue+(input[inputLength-k-i-1]*coeffs[i]);
+		}
+		output[k]=outputValue;
+		outputValue=0;
+	}
+
+	return output;
 }
