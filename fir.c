f61db558347a730dc409246fbcedc86648546ab9
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-06-05 21:08:44 +0200

    Fix samples shift in C implementation

--- fir.c
+++ fir.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include "fir.h"
+int overflow=0;
 
 int to_q15(double x)
 {
-    return 32768*x;
-    
-   	if(x > 0.999969)
-    	return (int)(32768 * x - 0.5);
-	
-	return (int)(32768 * x + 0.5);
+   	return x*32768;
 }
 
 int fir(int sample, int *coeffs, int *sample_history, int coeffs_length)
 {
-    int output = sample*coeffs[0];
+    int output = ((long)sample*coeffs[0])>>15;
 
     for(int i=1;i<no_of_coeffs;i++)
     {
-        output = output + ((long)(sample_history[no_of_coeffs-i-1]*coeffs[i])>>15);
+        output += (sample_history[no_of_coeffs-i-1]*(long)coeffs[i])>>15;
     }
 
     return output;
@@ -25,29 +21,30 @@ int fir(int sample, int *coeffs, int *sample_history, int coeffs_length)
 
 int main()
 {
-    int coeffs_q15[no_of_coeffs]; //FIX
+    int coeffs_q15[no_of_coeffs]; 
 
     for(int i=0; i<no_of_coeffs; i++)
     {
         coeffs_q15[i] = to_q15(coeffs[i]);
     }
 
-    int sample = 32767;					
-    int sample_history[no_of_coeffs-1]; //FIX
+    int sample = 32768;					
+    int sample_history[no_of_coeffs-2]; 
 
-	for(int i=0;i<no_of_coeffs-1;i++) sample_history[i]=0;
+	for(int i=0;i<no_of_coeffs-2;i++) sample_history[i]=0;
 
     for(int i=0; i<no_of_coeffs; i++)
     {
 		int output;
+
 		if(i==0) output = fir(sample, coeffs_q15, sample_history, no_of_coeffs);
         else output = fir(0, coeffs_q15, sample_history, no_of_coeffs);
+        
+        printf("COEFF: %d \n", coeffs_q15[i]);
+        printf("Wynik: %d \n", output);
 
-        printf("Coeffs: %d \n", coeffs_q15[i]);
-        printf("Output: %d \n", output);
-
-		sample_history[i]=sample;
-		if(i>0) sample_history[i-1] = 0;
+        sample_history[no_of_coeffs-2-i]=sample;
+		if(i>0) sample_history[no_of_coeffs-i-1] = 0;
     }
 }
 
