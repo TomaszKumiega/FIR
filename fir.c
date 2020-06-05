e1873b0b8acc7c0f5882c178bc9dfec872a79b8d
Tomasz Kumiega <tomekkumiega@interia.pl>
2020-06-05 20:09:43 +0200

    Fix q15 multiplication

--- fir.c
+++ fir.c
@@ -3,10 +3,12 @@
 
 int to_q15(double x)
 {
-    if(x < 0.0)
-        return (int)(32768 * x - 0.5);
-
-    return (int)(32767 * x + 0.5);
+    return 32768*x;
+    
+   	if(x > 0.999969)
+    	return (int)(32768 * x - 0.5);
+	
+	return (int)(32768 * x + 0.5);
 }
 
 int fir(int sample, int *coeffs, int *sample_history, int coeffs_length)
@@ -15,7 +17,7 @@ int fir(int sample, int *coeffs, int *sample_history, int coeffs_length)
 
     for(int i=1;i<no_of_coeffs;i++)
     {
-        output = output + (sample_history[no_of_coeffs-i-1]*coeffs[i]);
+        output = output + ((long)(sample_history[no_of_coeffs-i-1]*coeffs[i])>>15);
     }
 
     return output;
@@ -23,15 +25,15 @@ int fir(int sample, int *coeffs, int *sample_history, int coeffs_length)
 
 int main()
 {
-    int coeffs_q15[no_of_coeffs];
+    int coeffs_q15[no_of_coeffs]; //FIX
 
     for(int i=0; i<no_of_coeffs; i++)
     {
         coeffs_q15[i] = to_q15(coeffs[i]);
     }
 
-    int sample = 32767;
-    int sample_history[no_of_coeffs-1];
+    int sample = 32767;					
+    int sample_history[no_of_coeffs-1]; //FIX
 
 	for(int i=0;i<no_of_coeffs-1;i++) sample_history[i]=0;
 
@@ -40,6 +42,7 @@ int main()
 		int output;
 		if(i==0) output = fir(sample, coeffs_q15, sample_history, no_of_coeffs);
         else output = fir(0, coeffs_q15, sample_history, no_of_coeffs);
+
         printf("Coeffs: %d \n", coeffs_q15[i]);
         printf("Output: %d \n", output);
 
