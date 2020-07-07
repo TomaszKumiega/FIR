df79e3f983ffe8d323e77b0e87fe16bebdb6732a
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2020-07-07 11:20:16 +0200

    Change implementations

--- FIR - C/main.c
+++ FIR - C/main.c
@@ -3,6 +3,7 @@
 const int no_of_coeffs = 242;
 int coeffs_q15[242];
 int sample_history_input[242];
+int sample[4500];
 
 int fir(int sample, int *coeffs, int *sample_history, int coeffs_length)
 {
@@ -23,28 +24,25 @@ int fir(int sample, int *coeffs, int *sample_history, int coeffs_length)
     }
 
     current_sample_delay+=1;
+    if(current_sample_delay==242) current_sample_delay=0;
 
     return output;
 }
 
 int main()
 {
-    int sample =0;
     int i;
 
     for(i=0;i<no_of_coeffs;i++) sample_history_input[i]=0;
 
     int output;
 
-    for(i=0; i<no_of_coeffs; i++)
+    for(i=0; i<4500; i++)
     {
         output=0;
 
-        if(i<1) sample = 32767;
-        else sample = 0;
+        output = fir(sample[i], coeffs_q15, sample_history_input, no_of_coeffs);
 
-        output = fir(sample, coeffs_q15, sample_history_input, no_of_coeffs);
-
-        output=output+1-1;
+        printf("%d", output);
     }
 }
--- FIR - DSPLIB/main.c
+++ FIR - DSPLIB/main.c
@@ -5,25 +5,22 @@
 
 DATA coeffs_q15[242];
 DATA sample_history_input[244];
-
+DATA sample[4500];
 
 void main( void ) 
 {
     int i;
 
-    DATA sample;
+
     DATA output;
 
     for(i=0;i<no_of_coeffs+2;i++) sample_history_input[i]=0;
 
-    for(i=0;i<no_of_coeffs;i++)
+    for(i=0;i<4500;i++)
     {
         output=0;
 
-        if(i==0) sample = 32767;
-        else sample =0;
-
-        fir(&sample, coeffs_q15, &output, sample_history_input, 1, no_of_coeffs);
+        fir(&sample[i], coeffs_q15, &output, sample_history_input, 1, no_of_coeffs);
 
         printf("Wynik: %d\n", coeffs_q15[i]-output);
 
