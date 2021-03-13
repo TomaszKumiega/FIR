d09ce180614f286b718ec8bd71a4d6775cd1ffe6
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-03-13 01:17:44 +0100

    Change filter2 function to filter

--- Filter_Response_To_Kronecker_Delta_Input.m
+++ Filter_Response_To_Kronecker_Delta_Input.m
@@ -1,10 +1,10 @@
 coeff=coeff();
 
-k=2:242;
-k=[0,k];
-x = double(kroneckerDelta(sym(k)));
+I=1:242;
+I(1)=0;
+x = double(kroneckerDelta(sym(I)));
 y = FIR(coeff,x);       %odpowiedź filtru własnej implementacji na deltę kroneckera
-y1= filter2(coeff,x);   %odpowiedź filtru na deltę kroneckera przy urzyciu matlabowej funkcji
+y1= filter(coeff,1,x);   %odpowiedź filtru na deltę kroneckera przy urzyciu matlabowej funkcji
 
 hold on; zoom on; grid on;
 stem(coeff,'b');
